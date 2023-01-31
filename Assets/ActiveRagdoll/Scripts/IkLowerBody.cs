56f64f44ea0fece885ce07ffd0c82c526dc1334c
David <davidkimighty@gmail.com>
2023-01-31 22:35:25 +0900

    #10 multi leg movement

---
+++ Assets/ActiveRagdoll/Scripts/IkLowerBody.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CollieMollie.ActiveRagdoll
+{
+    public class IkLowerBody : MonoBehaviour
+    {
+        #region Variable Field
+        [SerializeField] private Transform _body = null;
+        [SerializeField] private IkLeg _leftLeg = null;
+        [SerializeField] private IkLeg _rightLeg = null;
+        [SerializeField] private DominentLeg _dominentLeg = DominentLeg.Left;
+
+        private IkLeg _primaryLeg = null;
+        private IkLeg _secondaryLeg = null;
+        private bool _legMoving = false;
+        private float _leftRightDelay = 0.5f;
+
+        private IEnumerator _legMovement = null;
+
+        #endregion
+
+        #region Public Functions
+        public void Init()
+        {
+            _leftLeg.Init();
+            _rightLeg.Init();
+
+            if (_dominentLeg == DominentLeg.Left)
+            {
+                _primaryLeg = _leftLeg;
+                _secondaryLeg = _rightLeg;
+            }
+            else
+            {
+                _primaryLeg = _rightLeg;
+                _secondaryLeg = _leftLeg;
+            }
+        }
+
+        public void MoveLegs(IkLegMovementPreset preset)
+        {
+            if (_legMoving) return;
+            _legMoving = true;
+
+            _legMovement = LegMovement(preset, () =>
+            {
+                _legMoving = false;
+            });
+            StartCoroutine(_legMovement);
+        }
+
+        #endregion
+
+        #region Private Functions
+        private IEnumerator LegMovement(IkLegMovementPreset preset, Action done)
+        {
+            _primaryLeg.MoveLeg(_body, preset);
+
+            float delay = preset.StepDuration * _leftRightDelay;
+            yield return new WaitForSeconds(delay);
+
+            _secondaryLeg.MoveLeg(_body, preset);
+
+            done?.Invoke();
+        }
+
+        #endregion
+    }
+
+    public enum DominentLeg { Left, Right }
+    public enum LegMovementState { Contact, Low, Passing, High }
+}
---
+++ Assets/ActiveRagdoll/Scripts/IkRig.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CollieMollie.ActiveRagdoll
+{
+    [DisallowMultipleComponent]
+    public class IkRig : MonoBehaviour
+    {
+        #region Variable Field
+        [SerializeField] private IkLowerBody[] _lowerBodies = null;
+        [SerializeField] private IkLegMovementPreset _legPreset = null;
+
+        private float _rearDelay = 0.75f;
+
+        private IEnumerator _lowerBodyMovement = null;
+
+        #endregion
+
+        private void Awake()
+        {
+            for (int i = 0; i < _lowerBodies.Length; i++)
+                _lowerBodies[i].Init();
+        }
+
+        private void Start()
+        {
+            _lowerBodyMovement = LowerBodyMovement();
+            StartCoroutine(_lowerBodyMovement);
+        }
+
+        #region Private Field
+        private IEnumerator LowerBodyMovement()
+        {
+            float elapsedTime = 0f;
+            int index = 0;
+
+            while (true)
+            {
+                float delay = _legPreset.StepDuration * _rearDelay;
+                if (elapsedTime > delay)
+                {
+                    _lowerBodies[index++ % _lowerBodies.Length].MoveLegs(_legPreset);
+                    elapsedTime = 0;
+                }
+                elapsedTime += Time.fixedDeltaTime;
+
+                yield return new WaitForFixedUpdate();
+            }
+        }
+
+        #endregion
+    }
+}
